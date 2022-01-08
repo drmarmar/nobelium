1e672eec42bf0e60352e0a44c23fee8b0670b9a6
drmarmar <16604029+drmarmar@users.noreply.github.com>
2022-01-07 22:12:10 -0800

    config updates

--- blog.config.js
+++ blog.config.js
@@ -9,6 +9,7 @@ const BLOG = {
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
+  // darkBackground: '#222426',
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 7,
@@ -59,3 +60,4 @@ const BLOG = {
 }
 // export default BLOG
 module.exports = BLOG
+
